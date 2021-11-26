d087fed5bef667e5d3c2848399a1db10bc1ed9a6
kshitijofficial <52281964+kshitijofficial@users.noreply.github.com>
2021-11-26 20:42:12 +0530

    Update Adoption.sol

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -2,16 +2,16 @@
 
 pragma solidity >=0.5.0 <0.9.0;
 
-contract Adoption {
-    address[16] public adopters;
+contract Team {
+    address[16] public team;
 
-    function getAdopters() public view returns (address[16] memory) {
-        return adopters;
+    function getTeamPlayers() public view returns (address[16] memory) {
+        return team;
     }
 
-    function adopt(uint256 petId) public returns (uint256) {
-        require(petId >= 0 && petId <= 15);
-        adopters[petId] = msg.sender;
-        return petId;
+    function selectJerseyNumber(uint256 playerId) public returns (uint256) {
+        require(playerId >= 0 && playerId <= 15);
+        team[playerId] = msg.sender;
+        return playerId;
     }
 }
